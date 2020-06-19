8a0abcaa66274d7a7166cd8f54deb2ef7cf517bc
Theodore Keloglou <zf@sirodoht.com>
2020-06-19 23:24:38 +0100

    add piece work and piece results structs on p2p

--- src/p2p.rs
+++ src/p2p.rs
@@ -1,8 +1,9 @@
+use crossbeam::channel::{Receiver, Sender};
 use std::convert::TryInto;
 use std::net::IpAddr;
-use std::sync::mpsc::Sender;
 
 use crate::client;
+use crate::p2p;
 
 pub static PEER_ID_STRING: &str = "kjh29409k8hj0wgej6c1";
 
@@ -12,13 +13,40 @@ pub struct Peer {
     pub port: u16,
 }
 
-pub fn start_download_worker(p: Peer, info_hash: &[u8; 20], tx_p: Sender<String>) {
-    let this_thread_client: client::Client;
+pub struct PieceWork {
+    pub index: i64,
+    pub hash: [u8; 20],
+    pub length: i64,
+}
+
+pub struct PieceResult {
+    pub index: i64,
+    pub buf: Vec<u8>,
+}
+
+pub fn start_download_worker(
+    p: Peer,
+    info_hash: &[u8; 20],
+    work_snd: Sender<p2p::PieceWork>,
+    work_rcv: Receiver<p2p::PieceWork>,
+    result_snd: Sender<p2p::PieceResult>,
+    result_rcv: Receiver<p2p::PieceResult>,
+) {
+    let mut this_thread_client: client::Client;
     let peer_id: [u8; 20] = PEER_ID_STRING.as_bytes().try_into().unwrap();
     let peer_ip = p.ip;
     match client::new(p, peer_id, *info_hash) {
         Ok(client) => {
             this_thread_client = client;
+            this_thread_client.send_unchoke();
+            this_thread_client.send_interested();
+            println!("success in completing handshake and unchoking");
+
+            for piece in work_rcv.recv() {
+                if !this_thread_client.bitfield.has_piece(piece.index) {
+                    work_snd.send(piece);
+                }
+            }
         }
         Err(_) => {
             println!("DROP: peer ip: {}", peer_ip);
